517fefe9d919135d1fb559349c26ec30c3a9c254
Reniside <chinweikeprince95@gmail.com>
2025-04-05 20:25:43 +0100

    complete

--- components/NavbarLeft.tsx
+++ components/NavbarLeft.tsx
@@ -1,5 +1,4 @@
 import Image from "next/image";
-import Link from "next/link";
 import React from "react";
 import haroldImg from "../public/harold.svg";
 
@@ -15,7 +14,7 @@ const NavbarLeft = () => {
     <div>
       <button
         onClick={handleHomeClick}
-        className="flex gap-2 md:text-2xl text-[1.2rem] font-bold [text-shadow:1px_1px_3px_var(--color-gold)]"
+        className="flex gap-2 md:text-2xl cursor-pointer text-[1.2rem] font-bold [text-shadow:1px_1px_3px_var(--color-gold)]"
       >
         <Image src={haroldImg} alt="#" />
         <h1>Harold Capital</h1>
