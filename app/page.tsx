cdb6282c7f08f2ccf74ecc9e487ba02a9c41462c
Reniside <renrenxp1@gmail.com>
2025-05-05 13:08:34 +0100

    feat: revert changes

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,6 @@ import AboutUs from "@/components/AboutUs";
 import Believe from "@/components/Believe";
 import Contact from "@/components/Contact";
 import Hero from "@/components/Hero";
-import Map from "@/components/Map";
 import MoreInfo from "@/components/MoreInfo";
 
 export default function Home() {
@@ -23,9 +22,6 @@ export default function Home() {
         </section>
         <section id="contact">
           <Contact />
-        </section>
-        <section>
-          <Map />
         </section>
       </div>
     </>
